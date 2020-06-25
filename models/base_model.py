b470b92e4c3047be334ef1cfee26f8bf575428aa
laucavv <laucavv23@gmail.com>
2020-06-25 21:39:21 +0000

    add fuction to_dict

--- models/base_model.py
+++ models/base_model.py
@@ -17,7 +17,8 @@ class BaseModel():
         """
             Prints: [<class name>] (<self.id>) <self.__dict__>
         """
-        print("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
+        return("[{}] ({}) {}".format(self.__class__.__name__, self.id,
+                                     self.__dict__))
 
     def save(self):
         """
@@ -27,8 +28,11 @@ class BaseModel():
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        dict  = self.__dict__.copy()
-
-
-
-
+        """ returns a dictionary containing all keys/values
+            of __dict__ of the instance
+        """
+        dict = self.__dict__.copy()
+        dict['created_at'] = self.created_at.isoformat()
+        dict['updated_at'] = self.updated_at.isoformat()
+        dict['__class__'] = self.__class__.__name__
+        return dict
