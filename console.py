73d3dea802b1f4bf3d41e1e81c0838e377a15924
linkjavier <linkjavier@hotmail.com>
2020-06-26 12:29:38 -0500

    Added do_create() to console

--- console.py
+++ console.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Entry point of the command interpreter"""
 import cmd
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
@@ -22,6 +23,19 @@ class HBNBCommand(cmd.Cmd):
         """an empty line executes anything"""
 
         pass
+
+    def do_create(self, args):
+        if not args:
+            print("** class name missing **")
+        elif not args == "BaseModel":
+            print("** class doesn't exist **")
+        else:
+            new = BaseModel()
+            print(new.id)
+
+    def do_show(self, args):
+
+
 if __name__ == '__main__':
     console = HBNBCommand()
     console.cmdloop()
