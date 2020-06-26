b1e4cad775a0e89e90fef47746645eee2f31a9a1
laucavv <laucavv23@gmail.com>
2020-06-26 00:55:18 +0000

    directory engine

---
+++ models/engine/file_storage.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Class FileStorage """
+import json
+from models.base_model import BaseModel
+
+
+class FileStorage:
+    """
+        Serializes instances to a JSON file and deserializes
+        JSON file to instances:
+    """
+    __file_path = 'file.json'
+    __objects = {}
+
+    def __init__(self):
+        """ init"""
+        pass
+    def all(self):
+        """ returns the dictionary __objects """
+        return FileStorage.__objects
+    def new(self, obj):
+        """ sets in __objects the obj with
+             key <obj class name>.id
+        """
+        FileStorage.__objects["{}.{}".format(obj.__class__.__name__, obj.id)] = obj
+    def save(self):
+        dic = {}
+        for key, value in FileStorage.__objects.items():
+            dic.update({key: value.to_dict()})
+        dic_dumps = json.dumps(dic)
+        with open(FileStorage.__file_path, "w") as file:
+            file.write(dic_dumps)
