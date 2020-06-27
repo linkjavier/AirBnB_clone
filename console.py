4ba977474d5a7d7f616f4266f41ca1c3e7e8d1c1
linkjavier <linkjavier@hotmail.com>
2020-06-26 19:06:15 -0500

    Working on update method

--- console.py
+++ console.py
@@ -26,6 +26,10 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def do_create(self, args):
+        """
+            Creates a new instance of BaseModel,
+            saves it (to the JSON file) and prints the id.
+        """
         if not args:
             print("** class name missing **")
         elif not args == "BaseModel":
@@ -36,7 +40,7 @@ class HBNBCommand(cmd.Cmd):
             print(new.id)
 
     def do_show(self, args):
-        """ """
+        """ Prints the string representation of an instance """
         if not args:
             print("** class name missing **")
         else:
@@ -55,7 +59,8 @@ class HBNBCommand(cmd.Cmd):
                     print("** no instance found **")
 
     def do_destroy(self, args):
-        """ """
+        """ Deletes an instance """
+
         if not args:
             print("** class name missing **")
         else:
@@ -73,7 +78,8 @@ class HBNBCommand(cmd.Cmd):
                 else:
                     print("** no instance found **")
     def do_all(self, args):
-        """ """
+        """ Prints all string representation of all instances """
+
         if not args == "BaseModel" and len(args) > 0:
             print("** class doesn't exist **")
         else:
@@ -82,6 +88,40 @@ class HBNBCommand(cmd.Cmd):
             for value in obj_all.values():
                 list_all.append(str(value))
             print(list_all)
+
+    def do_update(self, args):
+        """ Updates an instance """
+
+        if not args:
+            print("** class name missing **")
+        else:
+            list_arg = args.split(" ")
+            if not list_arg[0] == "BaseModel":
+                print("** class doesn't exist **")
+            elif len(list_arg) == 1:
+                print("** instance id missing **")
+            else:
+                dic_obj = storage.all()
+                id_found = list_arg[0] + "." + list_arg[1]
+                if id_found in dic_obj:
+                    if len(list_arg) == 2:
+                        print("** attribute name missing **")
+                    elif len(list_arg) == 3:
+                        print("** value missing **")
+                    else:
+                        obj = dic_obj[id_found]
+                        setattr(obj, "Name", 'Directo')
+                        # setInDict(dic_obj, [obj, list_arg[2]], list_arg[3])
+                        # obj.list_arg[2] = list_arg[3]
+                        obj.save()
+
+
+
+                else:
+                    print("** no instance found **")
+
+
+
 
 if __name__ == '__main__':
     console = HBNBCommand()
