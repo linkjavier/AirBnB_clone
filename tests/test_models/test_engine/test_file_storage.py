a94faa265e80b8fac69d1c6c4a7ce9bfc700f30e
laucavv <laucavv23@gmail.com>
2020-06-30 20:25:41 +0000

    test storage

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""
+    Test for class Review Model
+"""
+import unittest
+import pep8
+from datetime import datetime
+from models.base_model import BaseModel
+from models.engine import file_storage
+from models.engine.file_storage import FileStorage
+
+
+class TestPep8B(unittest.TestCase):
+    """ Check for pep8 validation. """
+    def test_pep8(self):
+        """ test base and test_base for pep8 conformance """
+        style = pep8.StyleGuide(quiet=True)
+        file1 = 'models/engine/file_storage.py'
+        file2 = 'tests/test_models/test_engine/test_file_storage.py'
+        result = style.check_files([file1, file2])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warning).")
+
+
+class TestDocsB(unittest.TestCase):
+    """ Check for documentation. """
+    def test_module_doc(self):
+        """ check for module documentation """
+        self.assertTrue(len(file_storage.__doc__) > 0)
+
+    def test_class_doc(self):
+        """ Check for documentation """
+        self.assertTrue(len(file_storage.__doc__) > 0)
+
+    def test_method_docs(self):
+        """ Check for method documentation """
+        for func in dir(FileStorage):
+            self.assertTrue(len(func.__doc__) > 0)
+
+
+class TestFileStorage(unittest.TestCase):
+    """Class FileStorage """
+    def setUp(self):
+        self.storage_1 = FileStorage()
+
+    def check_instance(self):
+        """ Check the existence of instance """
+        self.assertIsInstance(self.storage_1, FileStorage)
+
+    def check_all(self):
+        """ Checks a correct dictionary """
+        dic = self.storage_1.all()
+        self.assertEqual(type(dic), dict)
+        self.assertIs(dic, self.storage_1._FileStorage__objects)
+
+if __name__ == '__main__':
+    unittest.main()
